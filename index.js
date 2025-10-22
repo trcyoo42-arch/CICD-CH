6d09e8c1b304db9417e6780ce5432df6e88a4077
C4yoo <trcyoo42@gmail.com>
2025-10-22 10:17:01 +0700

    Cahyo

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Landing page route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+  res.send('Hello World!')
+})
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 
